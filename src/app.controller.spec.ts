cf7c9792a06c51629f89a03c2eaacb4d583cc85f
KethellinPereira <kethellinpereira2018@outlook.com>
2025-04-10 09:22:17 -0300

    add import invalid

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ket } from 'crypto';
 
 describe('AppController', () => {
   let appController: AppController;
