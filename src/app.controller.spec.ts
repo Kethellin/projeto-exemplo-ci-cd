020f457a2d6757fb51bd64b4df619335c7c73aa9
KethellinPereira <kethellinpereira2018@outlook.com>
2025-04-10 09:19:54 -0300

    change text

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Quero uma lasanha"', () => {
+      expect(appController.getHello()).toBe('Quero uma lasanha');
     });
   });
 });
